320ac30ea325f77db8b652fb330b3f14be6db079
Natanael <natanaelsm0102@gmail.com>
2026-08-29 10:02:42 -0300

    chore: setup inicial do projeto
    - estrutura base do projeto
    - dependências iniciais
    - configuração básica do docker

---
+++ sophia/src/main/java/br/com/fatec/sophia/SophiaApplication.java
@@ -0,0 +1,13 @@
+package br.com.fatec.sophia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SophiaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SophiaApplication.class, args);
+	}
+
+}
---
+++ sophia/src/test/java/br/com/fatec/sophia/SophiaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.fatec.sophia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SophiaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
